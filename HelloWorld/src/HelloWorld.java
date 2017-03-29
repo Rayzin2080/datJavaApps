85d9124cafb6e855c2bfaefad6573386434081c3
Ryan Pacia <ryanpacia2080@gmail.com>
2017-03-29 18:47:02 -0400

    relocated java development workspace

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int myInt = 10;
+System.out.println("Hello world!");
+System.out.println(myInt);
+myInt += 4;
+System.out.println(myInt);
+	}
+
+}
